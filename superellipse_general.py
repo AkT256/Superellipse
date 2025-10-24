18d471a8830307894c1e78854e520726a85534dd
AkT256 <123782634+AkT256@users.noreply.github.com>
2025-10-24 03:10:22 +0300

    Update superellipse_general.py

--- superellipse_general.py
+++ superellipse_general.py
@@ -56,6 +56,7 @@ class SuperellipseApp(tk.Tk):
         self.var_ny = tk.DoubleVar(value=2)
         self.var_rot = tk.DoubleVar(value=0)
         self.var_steps = tk.IntVar(value=600)
+        self.var_linewidth = tk.DoubleVar(value=2)          # Толщина линии
 
         # Слайдеры
         self.add_slider(panel, "Полуось a", self.var_a, 20, 340, "{:.0f}")
@@ -93,4 +94,5 @@ class SuperellipseApp(tk.Tk):
 if __name__ == "__main__":
     app = SuperellipseApp()
     app.mainloop()
+
 
